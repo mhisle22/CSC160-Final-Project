b50d7833199c750af740e7bd725bbc2657fe865c
Mark Hisle <mhisle22@gmail.com>
2020-04-19 21:14:32 -0400

    added calculated column

--- tourism.js
+++ tourism.js
@@ -19,12 +19,17 @@ var initHeaders = function(countries) //Adds interactivity
     { 
         countries.sort(function(a,b)
         {
+            
             var aArrivals = Number(a.Arrivals);
             var bArrivals = Number(b.Arrivals);
+            
+            if(aArrivals == null && bArrivals != null) {return 1;}
+            if(aArrivals != null && bArrivals == null) {return -1;}
+            if(aArrivals == null && bArrivals == null) {return 0;}
+            
             if(aArrivals > bArrivals) {return -1}
             else if(aArrivals < bArrivals) {return 1}
-            else { return 0;}
-            
+            else { return 0;} 
         });
         
         clearTable();
@@ -38,6 +43,11 @@ var initHeaders = function(countries) //Adds interactivity
         {
             var aDeparts = a.Departures;
             var bDeparts = b.Departures;
+            
+            if(aDeparts == null && bDeparts != null) {return 1;}
+            if(aDeparts != null && bDeparts == null) {return -1;}
+            if(aDeparts == null && bDeparts == null) {return 0;}
+            
             if(aDeparts > bDeparts) {return -1}
             else if(aDeparts < bDeparts) {return 1}
             else { return 0;}
@@ -46,20 +56,6 @@ var initHeaders = function(countries) //Adds interactivity
         createTable(countries);
     });
     
-    d3.select("#imports")
-    .on("click",function()
-    { 
-        countries.sort(function(a,b)
-        {
-            var aImports = a.ImportPercentage;
-            var bImports = b.ImportPercentage;
-            if(aImports > bImports) {return -1}
-            else if(aImports < bImports) {return 1}
-            else { return 0;}
-        });
-        clearTable();
-        createTable(countries);
-    });
     
     d3.select("#expend")
     .on("click",function()
@@ -68,6 +64,11 @@ var initHeaders = function(countries) //Adds interactivity
         {
             var aExpend = a.Expenditures;
             var bExpend = b.Expenditures;
+            
+            if(aExpend == null && bExpend != null) {return 1;}
+            if(aExpend != null && bExpend == null) {return -1;}
+            if(aExpend == null && bExpend == null) {return 0;}
+            
             if(aExpend > bExpend) {return -1}
             else if(aExpend < bExpend) {return 1}
             else { return 0;}
@@ -76,20 +77,25 @@ var initHeaders = function(countries) //Adds interactivity
         createTable(countries);
     });
     
-    d3.select("#pop")
+   /* d3.select("#pop")
     .on("click",function()
     { 
         countries.sort(function(a,b)
         {
             var aItems = a.Population;
             var bItems = b.Population;
+            
+            if(aItems == null && bItems != null) {return 1;}
+            if(aItems != null && bItems == null) {return -1;}
+            if(aItems == null && bItems == null) {return 0;}
+            
             if(aItems > bItems) {return -1}
             else if(aItems < bItems) {return 1}
             else { return 0;}
         });
         clearTable();
         createTable(countries);
-    });
+    });*/
     
     d3.select("#immigrant")
     .on("click",function()
@@ -98,6 +104,11 @@ var initHeaders = function(countries) //Adds interactivity
         {
             var aItems = a.PercentageImmigrantPopulation;
             var bItems = b.PercentageImmigrantPopulation;
+            
+            if(aItems == null && bItems != null) {return 1;}
+            if(aItems != null && bItems == null) {return -1;}
+            if(aItems == null && bItems == null) {return 0;}
+            
             if(aItems > bItems) {return -1}
             else if(aItems < bItems) {return 1}
             else { return 0;}
@@ -113,6 +124,31 @@ var initHeaders = function(countries) //Adds interactivity
         {
             var aItems = a.Diplomat;
             var bItems = b.Diplomat;
+            
+            if(aItems == null && bItems != null) {return 1;}
+            if(aItems != null && bItems == null) {return -1;}
+            if(aItems == null && bItems == null) {return 0;}
+            
+            if(aItems > bItems) {return -1}
+            else if(aItems < bItems) {return 1}
+            else { return 0;}
+        });
+        clearTable();
+        createTable(countries);
+    });
+    
+    d3.select("#world")
+    .on("click",function()
+    { 
+        countries.sort(function(a,b)
+        {
+            var aItems = a.World;
+            var bItems = b.World;
+            
+            if(aItems == null && bItems != null) {return 1;}
+            if(aItems != null && bItems == null) {return -1;}
+            if(aItems == null && bItems == null) {return 0;}
+            
             if(aItems > bItems) {return -1}
             else if(aItems < bItems) {return 1}
             else { return 0;}
@@ -124,6 +160,7 @@ var initHeaders = function(countries) //Adds interactivity
 
 //creates the table using all tourism information
 var createTable = function(tourism) {
+    
     
     //Creates a row for each country
     var rows = d3.select("tbody")
@@ -139,34 +176,119 @@ var createTable = function(tourism) {
     
     //append arrivals
     rows.append("td")
-        .text(function(country){return numberWithCommas(country.Arrivals);});
+        .text(function(country){if(country.Arrivals == 0){return "N/A";} else{return numberWithCommas(country.Arrivals);}});
     
     //append departures
     rows.append("td")
-        .text(function(country){return numberWithCommas(country.Departures);});
-    
-    //append imports
-    rows.append("td")
-        .text(function(country){return country.ImportPercentage;});
+        .text(function(country){if(country.Departures == 0){return "N/A";} else{return numberWithCommas(country.Departures);}});
     
     //append expend
     rows.append("td")
-        .text(function(country){return numberWithCommas(country.Expenditures);});
+        .text(function(country){if(country.Expenditures == 0){return "N/A";} else{return numberWithCommas(country.Expenditures);}});
     
     //append population
+    /*rows.append("td")
+        .text(function(country){if(country.Population == 0){return "N/A";} else{return numberWithCommas(country.Population);}});
+    */
+    //append immigration stats
     rows.append("td")
-        .text(function(country){return numberWithCommas(country.Population);});
+        .text(function(country){if(country.PercentageImmigrantPopulation == 0){return "N/A";} else{return country.PercentageImmigrantPopulation;}});
     
     //append immigration stats
     rows.append("td")
-        .text(function(country){return country.PercentageImmigrantPopulation;});
+        .text(function(country){if(country.Diplomat == 0){return "N/A";} else{return country.Diplomat;}});
     
-    //append immigration stats
+    
+    //calculate worldliness, then append
+    //so, how we're gonna do this is by scaling everything to a percentage
+    //then average from there
+    //if something is 0, don't include it MIGHT CHANGE LATER
+    var arrivals = tourism.map(function(country){return Number(country.Arrivals);});
+    arrivals = arrivals.filter(n => n);
+    
+    var min = d3.min(arrivals);
+    var max = d3.max(arrivals);
+    
+    var arrivalScale = d3.scaleLinear()
+      .domain([min, max])
+      .range([0, 1]);
+    
+    //this is not the correct name, but i dont want to rewrite this every time
+    //Get over it.
+    arrivals = tourism.map(function(country){return Number(country.Departures);});
+    arrivals = arrivals.filter(n => n); //translation: make this get rid off unset indices
+    
+    var min = d3.min(arrivals);
+    var max = d3.max(arrivals);
+    
+    var departureScale = d3.scaleLinear()
+      .domain([min, max])
+      .range([0, 1]);
+    
+    arrivals = tourism.map(function(country){return Number(country.Expenditures);});
+    arrivals = arrivals.filter(n => n);
+    
+    var min = d3.min(arrivals);
+    var max = d3.max(arrivals);
+    
+    var expendScale = d3.scaleLinear()
+      .domain([min, max])
+      .range([0, 1]);
+    
+    /*arrivals = tourism.map(function(country){return Number(country.Population);});
+    arrivals = arrivals.filter(n => n);
+    
+    var min = d3.min(arrivals);
+    var max = d3.max(arrivals);
+    
+    var popScale = d3.scaleLinear()
+      .domain([min, max])
+      .range([0, 1]);*/
+    
+    
+    arrivals = tourism.map(function(country){return Number(country.PercentageImmigrantPopulation);});
+    arrivals = arrivals.filter(n => n);
+    
+    var min = d3.min(arrivals);
+    var max = d3.max(arrivals);
+    
+    var imScale = d3.scaleLinear()
+      .domain([min, max])
+      .range([0, 1]);
+    
+    arrivals = tourism.map(function(country){return Number(country.Diplomat);});
+    arrivals = arrivals.filter(n => n);
+    
+    var min = d3.min(arrivals);
+    var max = d3.max(arrivals);
+    
+    var dipScale = d3.scaleLinear()
+      .domain([min, max])
+      .range([0, 1]);
+    
+    //now, we can finally append the calculated field by scaling each variable
     rows.append("td")
-        .text(function(country){return country.Diplomat;});
+        .text(function(country)
+        {
+            var count = 0;
+            var total = 0.0;
+            if(country.Diplomat !== 0){total += dipScale(country.Diplomat); count++;}
+            if(country.Arrivals !== 0){total += arrivalScale(country.Arrivals); count++;}
+            if(country.Expenditures !== 0){total += expendScale(country.Expenditures); count++;}
+            if(country.PercentageImmigrantPopulation !== 0){total += imScale(country.PercentageImmigrantPopulation); count++;}
+            //if(country.Population !== 0){total += popScale(country.Population); count++;}
+            if(country.Departures !== 0){total += departureScale(country.Departures); count++;}
+        
+            //check if this country had no data, which shouldnt ever be true. But it feels good to check...
+            if(count === 0) {return 'N\A';}
+            
+            //add the variable so we can retrieve it later for sorting
+            country.World = total / count;
+        
+            //now let's finally calculate this bad boy
+            return total / count;
     
-    //TODO: calculate worldliness
-    rows.append("td");
+        });
 }
 
 
@@ -210,7 +332,7 @@ var loadDepartures = function(arrivals) {
         //console.log(arrivals);
         
         
-        loadImports(arrivals);
+        loadExpenditures(arrivals);
     }
 
     var failureFcn = function(errorMsg) //If there was an error
@@ -219,36 +341,6 @@ var loadDepartures = function(arrivals) {
     }
 
     promise2.then(successFcn,failureFcn);
-    
-}
-
-var loadImports = function(tourism) {
-
-    var promise3 = d3.csv("total_imports.csv"); //Promise to get the data
-
-    var successFcn = function(imports) //If the data is successfully collected
-    {
-        //console.log("Data Collected:", imports);
-        
-        //outer left join of all data with imports
-        tourism.forEach(function(country) {
-            var result = imports.filter(function(importCountry) {
-                return importCountry.Country === country.Country;
-            });
-            delete imports.Country;
-            country.ImportPercentage = Number((result[0] !== undefined) ? result[0].ImportPercentage : null);
-        });
-        //console.log(tourism);
-
-        loadExpenditures(tourism);
-    }
-
-    var failureFcn = function(errorMsg) //If there was an error
-    {
-        console.log("Something went wrong",errorMsg);
-    }
-
-    promise3.then(successFcn,failureFcn);
     
 }
 
